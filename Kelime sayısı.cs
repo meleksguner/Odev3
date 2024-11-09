d571dda9ab031499f78b2a83028ccd5e5f4b5a7a
Melek <msenaguner34@gmail.com>
2024-11-09 17:38:53 +0300

    Add files via upload

---
+++ Kelime sayısı.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Bir cümle giriniz: ");
+        string cumle = Console.ReadLine();
+
+
+        string[] kelimeler = cumle.Split(' ');
+
+        
+        int kelimeSayisi = kelimeler.Length;
+
+        
+        Console.WriteLine("Cümledeki kelime sayısı: " + kelimeSayisi);
+    }
+}
+
