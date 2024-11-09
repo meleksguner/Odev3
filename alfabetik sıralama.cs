b6d6978e50de4d4c3e253c66a962f692777c4e16
Melek <msenaguner34@gmail.com>
2024-11-09 18:00:10 +0300

    Add files via upload

---
+++ alfabetik sıralama.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        
+        ArrayList isimler = new ArrayList();
+
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Bir isim girin: ");
+            string isim = Console.ReadLine();
+            isimler.Add(isim);
+        }
+
+        
+        isimler.Sort();
+
+        
+        Console.WriteLine("\nAlfabetik sıralama:");
+        foreach (string isim in isimler)
+        {
+            Console.WriteLine(isim);
+        }
+    }
+}
