148e4215f0478fc1663acce290435d4fbca8d5e1
Melek <msenaguner34@gmail.com>
2024-11-10 14:41:11 +0300

    Add files via upload

---
+++ Tahmin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        Random sayi = new Random();
+        int hedef_sayi = sayi.Next(1, 100);
+        int tahmin = 0;
+
+        Console.WriteLine("Aklınızdan 1 ile 100 arasından bir sayı tutunuz. Haydi tahmin edelim!!");
+        
+        while (hedef_sayi != tahmin)
+        {
+            Console.WriteLine("Tahmininiz:");
+            tahmin = Convert.ToInt32(Console.ReadLine());
+
+
+            if (tahmin < hedef_sayi)
+            {
+                Console.WriteLine("Daha büyük sayı giriniz!");
+
+            }
+            else if (tahmin > hedef_sayi)
+            {
+                Console.WriteLine("Daha küçük sayı giriniz!");
+
+            }
+            else
+            {
+                Console.WriteLine("Tebrikler doğru tahmin");
+            }
+        }
+        
+        Console.ReadLine();
+    }
+
+}
+
