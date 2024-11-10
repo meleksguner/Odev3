1469316d6d310b76abba56799b87020ce7e8c1fd
Melek <msenaguner34@gmail.com>
2024-11-10 23:59:31 +0300

    Add files via upload

---
+++ Toplama oyunu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int toplam = 0;
+        string sayilar = ""; // Girilen sayıları saklamak için bir string
+
+        while (true)
+        {
+            Console.Write("Pozitif bir tam sayı girin (Çıkmak için 0 girin): ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            if (sayi == 0)
+            {
+                break; // Giriş 0 ise döngüden çık
+            }
+            else if (sayi > 0)
+            {
+                toplam += sayi; // Sayıyı toplama ekle
+                sayilar += sayi + " "; // Sayıyı string değişkene ekle
+            }
+            else
+            {
+                Console.WriteLine("Lütfen sadece pozitif tam sayılar girin.");
+            }
+        }
+
+        Console.WriteLine("\nGirilen pozitif sayıların toplamı: " + toplam);
+        Console.WriteLine("Girilen pozitif sayılar: " + sayilar);
+    }
+}
+
