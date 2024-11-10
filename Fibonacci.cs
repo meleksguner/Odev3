2cb84a062f7befc940ebfbc928e25cdec35c1b19
Melek <msenaguner34@gmail.com>
2024-11-10 14:01:49 +0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bir sayı giriniz.");
+        
+        int n= Convert.ToInt32(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+        int c;
+
+        Console.WriteLine(a + "\n" + b);
+        for (int i=2; i<n; i++)
+        {
+            c = a + b;
+            Console.WriteLine(c);
+            a = b;
+            b=c;
+
+        }
+        Console.ReadLine();
+
+
+
+    }
+}
